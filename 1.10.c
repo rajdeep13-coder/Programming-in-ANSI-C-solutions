02a2fa6b98aea715f87f5d3086d90f99fe7caeb9
Rajdeep Paul <rajdeeppaul427@gmail.com>
2025-02-08 20:50:39 +0530

    Update 1.10.c

--- 1.10.c
+++ 1.10.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    int a,b,c,s;
-    int r,area;
-    printf("Enter values of a, b and c: ");
-    scanf("%d, %d , %d", &a, &b, &c);
-    s=(a+b+c)/2;
-    r=s*(s-a)*(s-b)*(s-c);
-    area=r.sqrt();
-    printf("\nArea of the triangle:%d ", area);
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    double a, b, c, s, area;
+    printf("Enter the lengths of the sides of the triangle (a, b, c): ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    s = (a + b + c) / 2;
+    area = sqrt(s * (s - a) * (s - b) * (s - c));
+    printf("The area of the triangle is: %.2f\n", area);
+    
+    return 0;
 }
