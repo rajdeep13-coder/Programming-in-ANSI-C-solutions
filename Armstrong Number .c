9b9e687ec0e8e796f18b297297312114f100f44d
Rajdeep Paul <rajdeeppaul427@gmail.com>
2025-02-14 19:05:30 +0530

    Update Armstrong Number .c

--- Armstrong Number .c
+++ Armstrong Number .c
@@ -7,14 +7,14 @@ int main() {
     scanf("%d", &n);
     temp = n;
     while (temp != 0) {
-        temp /= 10;
-        digits++;
+        temp= temp/10;
+        digits= digits+1;
     }
     temp = n;
     while (temp != 0) {
         rem = temp % 10;
-        sum += pow(rem, digits);
-        temp /= 10;
+        sum = sum + pow(rem, digits);
+        temp= temp/10;
     }
     if (sum == n) {
         printf("%d is an Armstrong number.\n", n);
