766ebf353e7d123ebd817d8459919026611cc3db
Rajdeep Paul <180819935+rajdeep13-coder@users.noreply.github.com>
2025-02-17 12:33:46 +0530

    Create 3.14 .c

---
+++ 3.14 .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int m, n;
+    printf("Enter two integers (m and n): ");
+    scanf("%d %d", &m, &n);
+    if (n != 0 && m % n == 0) {
+        printf("%d is a multiple of %d\n", m, n);
+    } else {
+        printf("%d is not a multiple of %d\n", m, n);
+    }
+    return 0;
+}
